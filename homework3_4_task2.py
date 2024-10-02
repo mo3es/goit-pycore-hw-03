9772e8b01870ca5f56620232525f644e3154582e
mo3es <86818931+mo3es@users.noreply.github.com>
2024-10-02 11:51:56 +0300

    Update homework3_4_task2.py

--- homework3_4_task2.py
+++ homework3_4_task2.py
@@ -1,16 +1,31 @@
-import random
+iimport random
+
 
 def get_numbers_ticket(min, max, quantity: int) -> set:
+
+    # Creation of collection, that would contain only unique values
     result = set()
+
+    # Input validation
     try:
         min, max, quantity = int(min), int(max), int(quantity)
-        if (min < 1 or min > 999 or min >= max) or (max > 1000 or max < 2) or (quantity > max - min):
+        if (
+            (min < 1 or min > 999 or min >= max)
+            or (max > 1000 or max < 2)
+            or (quantity > max - min)
+        ):
             raise ValueError
     except (TypeError, ValueError):
         print("Incorrect type/value of inputted numbers")
         return result
+    # Random results generation until get predefined quantity
     else:
         while len(result) < quantity:
             next_random = random.randint(min, max)
             result.add(next_random)
     return sorted(result)
+
+
+# Usage example
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
